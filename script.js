076e37c79656bd7ae92ef95bffd37104a2e357ee
M1783174 <79620198+M1783174@users.noreply.github.com>
2021-05-14 11:51:27 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
-const quotes = [
-    {
-        name:'boy',
-        quote:'i am a boy and also a student.'
-    },
-    {
-        name:'girl',
-        quote:'i am a girl and also a student'
-    },
-    {
-        name:'mother',
-        quote:'i love my child very......i dont love them hahahah'
-    },
-    {
-        name:'father',
-        quote:'i dont love my child'
-    },
+const colorBtn = document.querySelector('.colorBtn');
+const bodyBcg = document.querySelector('body');
 
-]
+const color = ['yellow','purple','green','#3b5998'];
 
-const quoteBtn = document.querySelector('#quoteBtn');
-const quoteAuthor = document.querySelector('#quoteAuthor');
-const quote = document.querySelector('#quote');
-
-quoteBtn.addEventListener("click",displayQuote);
-
-function displayQuote (){
-    let number = Math.floor(Math.random()*quote.length);
-    quoteAuthor.innerHTML = quotes[number].name;
-    quote.innerHTML = quotes[number].quote;
+colorBtn.addEventListener('click',changeColor);
+function changeColor (){
+	let random = Math.floor(Math.random()*color.length)
+	bodyBcg.style.backgroundColor = color[random];
 }
